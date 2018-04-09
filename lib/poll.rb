4a66b729d441c95a48cf9a339f0b7f963499c061
Devin Gaffney <itsme@devingaffney.com>
2018-04-09 14:48:32 -0400

    Udpates

--- lib/poll.rb
+++ lib/poll.rb
@@ -9,7 +9,19 @@ class Poll
   
   def store_latest_comments
     lc = latest_comments
-    binding.pry
+    times = {}
+    lc.each do |c|
+      times[c["author"]] ||= []
+      times[c["author"]] << c["created_utc"]
+    end;false
+    found_authors = Hash[$client[:reddit_authors].find({"author" => {"$in" => times.keys}}).to_a.collect{|x| [x["author"], x]}]
+    lc.collect{|c| c["author"]}.counts.each do |author, count|
+      if found_authors[author].nil?
+        $client[:reddit_authors].insert_one({author: author, "first_comment_seen" => times[author].sort.first})
+      end
+      $client[:reddit_authors].update_one({author: author}, {"$inc" => {"comment_count" => count}})
+      $client[:reddit_authors].update_one({author: author}, {"last_comment_seen" => times[author].sort.last})
+    end
     $client[:reddit_comments].insert_many(lc, ordered: false)
   end
 
@@ -23,6 +35,19 @@ class Poll
   
   def store_latest_submissions
     ls = latest_submissions
+    times = {}
+    ls.each do |s|
+      times[s["author"]] ||= []
+      times[s["author"]] << s["created_utc"]
+    end;false
+    found_authors = Hash[$client[:reddit_authors].find({"author" => {"$in" => times.keys}}).to_a.collect{|x| [x["author"], x]}]
+    ls.collect{|s| s["author"]}.counts.each do |author, count|
+      if found_authors[author].nil?
+        $client[:reddit_authors].insert_one({author: author, "first_submission_seen" => times[author].sort.first})
+      end
+      $client[:reddit_authors].update_one({author: author}, {"$inc" => {"submission_count" => count}})
+      $client[:reddit_authors].update_one({author: author}, {"last_submission_seen" => times[author].sort.last})
+    end
     $client[:reddit_submissions].insert_many(ls, ordered: false)
   end
 end
