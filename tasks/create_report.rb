47ca4aa24e2fc038e2e04dce3fcddddbb14758cc
Devin Gaffney <itsme@devingaffney.com>
2018-04-15 15:32:10 -0400

    updates

--- tasks/create_report.rb
+++ tasks/create_report.rb
@@ -1,6 +1,7 @@
 class CreateReport
   include Sidekiq::Worker
   def perform(cursor)
+    cursor = Time.at(cursor)
     report = Report.snapshot(cursor)
     $client[:stats].insert_one(time: cursor, content: report)
   end
