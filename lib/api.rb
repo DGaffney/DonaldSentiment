f84ae2f0617f65937d001fb646dbaf6fb75e9ecc
Devin Gaffney <itsme@devingaffney.com>
2018-04-16 14:48:23 -0400

    updates

--- lib/api.rb
+++ lib/api.rb
@@ -6,6 +6,6 @@ class Site < Sinatra::Base
   end
 
   get "/api/latest/:content_type/reduced.json" do
-    return Report.report(Time.now, params[:content_type], true).sort_by{|k,v| k}.to_json
+    return {type: params[:content_type], data: Report.report(Time.now, params[:content_type], true).sort_by{|k,v| k}}.to_json
   end
 end
