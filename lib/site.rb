8a320225c08af351a4c7c0f7c5d979d0a9c4184f
Devin Gaffney <itsme@devingaffney.com>
2018-04-16 08:54:42 -0400

    updates

--- lib/site.rb
+++ lib/site.rb
@@ -1,5 +1,5 @@
 require 'sinatra/base'
-set :root, File.join(File.dirname(__FILE__), '..')
+set :root, File.join(File.dirname(__FILE__), '../views/')
 # sets the view directory correctly
 set :views, Proc.new { File.join(root, "views") } 
 
