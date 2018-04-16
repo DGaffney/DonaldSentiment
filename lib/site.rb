31357efae69cbfd282c48fc365f3d148707d6f06
Devin Gaffney <itsme@devingaffney.com>
2018-04-16 08:49:13 -0400

    updates

--- lib/site.rb
+++ lib/site.rb
@@ -1,15 +1,7 @@
 require 'sinatra/base'
 
 class Site < Sinatra::Base
-  get "/latest.json" do
-    return Report.latest.to_json
-  end
-
-  get "/current.json" do
-    return Report.current.to_json
-  end
-
-  get "/previous.json" do
-    return Report.latest.to_json
+  get '/' do
+    erb :index
   end
 end
