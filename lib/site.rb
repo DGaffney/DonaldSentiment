74fbf35805cca1f5d2ffaa368d996b042a7b078e
Devin Gaffney <itsme@devingaffney.com>
2018-04-17 00:15:03 -0400

    updates

--- lib/site.rb
+++ lib/site.rb
@@ -3,12 +3,11 @@ require 'sinatra/base'
 class Site < Sinatra::Base
 
   get '/' do
-  if params[:timestamp].nil?
-    @time = Time.now.to_i
-  else
-    @time = params[:timestamp]
-  end
-
+    if params[:time].nil?
+      @time = Time.now.to_i
+    else
+      @time = params[:time]
+    end
     erb :index
   end
 end
