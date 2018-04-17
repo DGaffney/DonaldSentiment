a60cdfcda954e9cf24cd604707b6c9a7e359ab2b
Devin Gaffney <itsme@devingaffney.com>
2018-04-17 10:05:57 -0400

    updates

--- lib/site.rb
+++ lib/site.rb
@@ -5,8 +5,10 @@ class Site < Sinatra::Base
   get '/' do
     if params[:time].nil?
       @time = Time.now.to_i
+      @distance = 1
     else
       @time = params[:time].to_i
+      @distance = params[:distance].to_i
     end
     erb :index
   end
